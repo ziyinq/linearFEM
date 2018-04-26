d7d22bfa98ea92c97599b019a748838eacd5a1bf
ziyin <roshomon.vol@gmail.com>
2018-04-26 14:28:06 -0400

    get some linear data

--- src/main.cpp
+++ src/main.cpp
@@ -10,9 +10,12 @@ int main()
     // FemSimulation<T, dim> sim;
     // sim.createMesh();
     T dt = 1e-3;
-    T Youngs = 1.5e4;
-    FemQuadSim<T, dim> quadSim(dt, Youngs);
+    T Youngs = 1.1e5;
+
+    // FemQuadSim<T, dim> quadSim(dt, Youngs);
+    // quadSim.startSimulation();
+
     FemSimulation<T, dim> linearSim(dt, Youngs);
     linearSim.startSimulation();
-    quadSim.startSimulation();
+
 }
